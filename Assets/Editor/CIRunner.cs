fefdefcc851c6b5460380a451864faf94b8c01ec
Genteure <genteure@gmail.com>
2018-06-12 13:31:00 +0800

    Add CI

---
+++ Assets/Editor/CIRunner.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+
+class CIRunner
+{
+    [MenuItem("Build/Windows Build")]
+    static void Build()
+    {
+        string path = "build";
+        string[] scenes = new string[] { "Assets/bilibili-live.unity" };
+
+        FileUtil.DeleteFileOrDirectory(path);
+
+        BuildPipeline.BuildPlayer(scenes, path + "/BililiveGiftCan.exe", BuildTarget.StandaloneWindows64, BuildOptions.CompressWithLz4HC);
+
+        // FileUtil.DeleteFileOrDirectory(path + "/UnityCrashHandler64.exe");
+        // new DirectoryInfo(path + "/Data/Managed").EnumerateFiles("*.xml").ToList().ForEach(x => x.Delete());
+    }
+}
